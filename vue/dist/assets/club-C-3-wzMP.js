af469b8b5439d165a434582f07b0ae5f9f24b01c
sangsang <1433174509@qq.com>
2026-05-04 14:31:46 +0800

    移除不改上传的文件

--- vue/dist/assets/club-C-3-wzMP.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./request-C7c6yK2x.js";function u(){return e({url:"/api/club/verified",method:"get"})}function i(r){return e({url:`/api/club/${r}`,method:"get"})}export{i as a,u as g};
